6617b4e919458fa6287b91d1ad34ad0bb55d0144
leonidbogin <i@leonidbogin.com>
2019-10-05 01:42:14 +0300

    Large revision of the interface

--- AutoGarmin/View/UserControlDevice.xaml.cs
+++ AutoGarmin/View/UserControlDevice.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +25,45 @@ namespace AutoGarmin
         {
             InitializeComponent();
             this.Name = Name;
-            LabelModel.ToolTip = "Device id: " + device.id;
             LabelModel.Content = device.model + " (" + device.diskname + ")";
             LabelNickname.Content = device.nickname;
+            LabelId.Content = device.id;
+            LabelTimeConnect.Content = device.timeConnect.ToString("HH:mm:ss");
+            ImageDevice.Source = LoadIco(device.diskname);
+        }
+
+        private BitmapImage LoadIco(string diskname)
+        {
+            BitmapImage bm1 = new BitmapImage();
+
+            string path = diskname + @"\Garmin\Garmintriangletm.ico";
+            if (!File.Exists(path))
+            {
+                List<string> files = new List<string>();
+                try
+                {
+                    GetAllFiles(diskname + "/", "*.ico", files);
+                }
+                catch (UnauthorizedAccessException) { }
+                if (files.Count > 0)
+                    path = files[0];
+                else path = "pack://application:,,,/no.ico";
+            }
+            bm1.BeginInit();
+            bm1.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+            bm1.CacheOption = BitmapCacheOption.OnLoad;
+            bm1.EndInit();
+
+            return bm1;
+        }
+
+        private static void GetAllFiles(string rootDirectory, string fileExtension, List<string> files)
+        {
+            string[] directories = Directory.GetDirectories(rootDirectory);
+            files.AddRange(Directory.GetFiles(rootDirectory, fileExtension));
+
+            foreach (string path in directories)
+                GetAllFiles(path, fileExtension, files);
         }
     }
 }
