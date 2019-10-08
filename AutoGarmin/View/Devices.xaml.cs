85c6b3a6414644239b2fa3d6a7bbdb49b7d60889
leonidbogin <i@leonidbogin.com>
2019-10-08 15:05:15 +0300

    big feat
    feat: add sound error
    feat: manual start of auto mode
    feat: auto mode works with already connected devices
    refactor: auto method moved to Device Control

--- AutoGarmin/View/Devices.xaml.cs
+++ AutoGarmin/View/Devices.xaml.cs
@@ -74,90 +74,6 @@ namespace AutoGarmin.View
         }
         #endregion
 
-        #region Auto
-        public void Auto(object _device)
-        {
-            Device device = (Device)_device;
-            //Copy tracks
-            if (Properties.Settings.Default.AutoTracksDownload)
-            {
-                if (Directory.Exists(device.diskname + Const.Path.GPXPath))
-                {
-                    string pathTarget = Const.Path.GPX + @"\" + DateTime.Now.ToShortDateString()
-                        + @"\(" + DateTime.Now.ToString(Const.Time.Folder) + ")";
-                    if (device.nickname != null && device.nickname.Length > 0)
-                        pathTarget += $" - ({device.nickname})";
-                    if (device.model != null && device.model.Length > 0)
-                        pathTarget += $" - ({device.model})";
-                    pathTarget += $" - ({device.id})";
-                    DirectoryInfo dirSource = new DirectoryInfo(device.diskname + Const.Path.GPXPath);
-                    DirectoryInfo dirTrarget = new DirectoryInfo(pathTarget);
-                    CopyAll(dirSource, dirTrarget);
-                }
-                else
-                {
-                    Sound.Play(Const.Path.Sound.Error);
-                    logs.Add(device, Const.Error.NoTrack);
-                }
-            }
-            //Clean tracks
-            if (Properties.Settings.Default.AutoTracksClean)
-            {
-                if (Directory.Exists(device.diskname + Const.Path.GPXPath))
-                { 
-                    DirectoryInfo dir = new DirectoryInfo(device.diskname + Const.Path.GPXPath);
-                    DeleteAll(dir);
-                }
-            }
-            //End
-            device.ready = true;
-            Sound.Play(Const.Path.Sound.Ready);
-            this.Dispatcher.Invoke((System.Threading.ThreadStart)delegate {
-                device.control.RectangleDeviceStatus.Fill = new SolidColorBrush(
-                    Color.FromRgb(11, 89, 141));
-            });
-        }
-
-        public static void CopyAll(DirectoryInfo source, DirectoryInfo target) //Копирование папки
-        {
-            // Если директория для копирования файлов не существует, то создаем ее
-            if (Directory.Exists(target.FullName) == false)
-            {
-                Directory.CreateDirectory(target.FullName);
-            }
-
-            // Копируем все файлы в новую директорию
-            foreach (FileInfo fi in source.GetFiles())
-            {
-                fi.CopyTo(System.IO.Path.Combine(target.ToString(), fi.Name), true);
-            }
-
-            // Копируем рекурсивно все поддиректории
-            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-            {
-                // Создаем новую поддиректорию в директории
-                DirectoryInfo nextTargetSubDir =
-                  target.CreateSubdirectory(diSourceSubDir.Name);
-                // Опять вызываем функцию копирования
-                // Рекурсия
-                CopyAll(diSourceSubDir, nextTargetSubDir);
-            }
-        }
-
-        public static void DeleteAll(DirectoryInfo source) //Копирование папки
-        {
-            foreach (FileInfo file in source.GetFiles())
-            {
-                file.Delete();
-            }
-
-            foreach (DirectoryInfo dir in source.GetDirectories())
-            {
-                dir.Delete(true);
-            }
-        }
-        #endregion
-
         public void Add(string id, string nickname, string diskname, string model) //Добавление устройства
         {
             Device device = new Device()
@@ -176,9 +92,7 @@ namespace AutoGarmin.View
             if (AutoWork)
             {
                 //Запуск авто режима
-                System.Threading.Thread myThread = new System.Threading.Thread(
-                    new System.Threading.ParameterizedThreadStart(Auto));
-                myThread.Start(device);
+                device.control.StartAuto();
             }
         }
     }
