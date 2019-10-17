b47832f2b96cdd535bb83182033eac1449721310
leonidbogin <ileonidbogin@gmail.com>
2019-10-17 23:06:55 +0300

    refactor: UserControlUSB

--- AutoGarmin/AutoGarmin/View/UserControlMaps.xaml.cs
+++ AutoGarmin/AutoGarmin/View/UserControlMaps.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,55 @@ namespace AutoGarmin.View
     /// </summary>
     public partial class UserControlMaps : UserControl
     {
+        private ObservableCollection<MapFile> files = new ObservableCollection<MapFile>();
+
+        public class MapFile
+        {
+            public string DisplayName { get; set; }
+            public string DisplaySize { get; set; }
+            public string DisplayTooltip { get; set; }
+            public Visibility DisplayClose { get; set; }
+        }
+
         public UserControlMaps()
         {
             InitializeComponent();
+            DataGridFiles.ItemsSource = files;
+
+            MapFile file = new MapFile()
+            {
+                DisplayName = "zal.kmz",
+                DisplaySize = "3.5 Мб",
+                DisplayTooltip = "zal.kmz (3.5 Мб) 15.10.2019 16:23:31",
+                DisplayClose = Visibility.Hidden
+            };
+            MapFile file2 = new MapFile()
+            {
+                DisplayName = "grid.kmz",
+                DisplaySize = "5 Мб",
+                DisplayTooltip = "zal.kmz (3.5 Мб) 15.10.2019 16:23:31",
+                DisplayClose = Visibility.Hidden
+            };
+
+            files.Add(file);
+            files.Add(file2);
+            DataGridFiles.ScrollIntoView(files.Last());
+        }
+
+        private void DataGridDelete(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("delete");
+        }
+
+        private void DataGridFiles_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            
+        }
+
+        private void DataGridFiles_SelectedCellsChanged_1(object sender, SelectedCellsChangedEventArgs e)
+        {
+            int i = DataGridFiles.SelectedIndex;
+            files[0].DisplayClose = Visibility.Visible;
         }
     }
 }
