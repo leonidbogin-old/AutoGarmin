07377ae9fae11dd43d97e47347bb20e27c6230d7
Leonid Boginskiy <i@leonidbogin.com>
2019-11-05 03:55:03 +0300

    big feat: add the interface of connected devices
    add no.ico
    add class DeviceInfo
    add class Const

---
+++ AutoGarmin/AutoGarmin/Class/DeviceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGarmin.Class
+{
+    public class DeviceInfo //device information
+    {
+        public string id { get; set; } //id device
+        public string nickname { get; set; } //name
+        public string model { get; set; } //model
+        public string diskname { get; set; } //drive letter
+        public string icon { get; set; }
+
+        public DateTime timeConnect { get; set; }
+    }
+}
